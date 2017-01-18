963d13503440ea143603a9ed8314eb11215af58c
davidasherwilson <davidasherwilson@gmail.com>
2017-01-18 16:45:12 -0500

    Add files via upload

---
+++ testJavascript.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    var myArray = [];
+    var getButton = document.getElementById("btn");
+    var randomNum;
+    
+    getButton.onclick = clickFunction;
+    
+    function clickFunction () {
+        randomNum = Math.floor(Math.random() * 10) + 1;
+        myArray.unshift(randomNum);
+        console.log(myArray);
+    }
+    
+    
+}//end of onload function
